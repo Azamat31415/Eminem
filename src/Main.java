e318e6f174421aacc561b3e709ee78404e069201
azabr <230147@astanait.edu.kz>
2024-01-21 13:00:40 +0600

    Enter characteristics about students and employees

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,10 @@ import modules.Student;
 import java.util.ArrayList;
 import java.util.Collections;
 public class Main {
+    public static void main(String[] args) {
+        Employee Employee1 = new Employee("Patrick", "Bateman", 29666.910);
+        Employee Employee2 = new Employee("Marshall", "Mathers", 50000.00);
+        Student Student1 = new Student("Ryan", "Gosling", 2.66);
+        Student Student2 = new Student("Peter", "Parker", 4.00);
+    }
 }
